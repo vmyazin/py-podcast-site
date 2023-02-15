e841824baacde950ab808c1cbfc0d8f00778b10b
Vasily Myazin <vm@konspiredesign.com>
2023-02-15 17:24:48 -0300

    Add better page styling with artwork

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-import sys
-sys.path.append('/var/www/py-podcast-site/config')
+#import sys
+#sys.path.append('/var/www/py-podcast-site/config')
 
 import xml.etree.ElementTree as ET
 import requests
+import datetime
 from flask import Flask, render_template, request, redirect, url_for
 #from config.podcasts_list import PODCASTS
 PODCASTS = [
@@ -15,28 +16,11 @@ PODCASTS = [
 
 app = Flask(__name__)
 
-@app.route('/load_xml/<url>', methods=['GET'])
-def load_xml(url):
-    print("Load XML Endpoint Executed") # Debugging statement
-    response = requests.get(url)
-    xml_data = response.content
-
-    root = ET.fromstring(xml_data)
-
-    # Get the podcast title
-    title = root.find('channel/title').text
-
-    # Get the list of episodes
-    episodes = []
-    for item in root.findall('channel/item'):
-        date = item.find('pubDate').text
-        episode_title = item.find('title').text
-        episodes.append((date, episode_title))
-
-    return render_template('index.html', title=title, episodes=episodes, podcasts=PODCASTS)
-
 @app.route('/', methods=['GET', 'POST'])
+
 def index():
+    data = {}
+
     if request.method == 'POST':
         url = request.form['url']
         response = requests.get(url)
@@ -44,19 +28,23 @@ def index():
 
         root = ET.fromstring(xml_data)
 
-        # Get the podcast title
-        title = root.find('channel/title').text
+        data["title"] = root.find('channel/title').text
+        data["image_url"] = root.find('channel/image/url').text
+        data["description"] = root.find('channel/description').text
 
         # Get the list of episodes
-        episodes = []
+        data["episodes"] = []
         for item in root.findall('channel/item'):
             date = item.find('pubDate').text
+            try:
+                date = datetime.datetime.strptime(date, '%a, %d %b %Y %H:%M:%S %z')
+                date = date.strftime("%B %d, %Y")
+            except ValueError:
+                date = "Invalid Date"
             episode_title = item.find('title').text
-            episodes.append((date, episode_title))
+            data["episodes"].append((date, episode_title))
 
-        return render_template('index.html', title=title, episodes=episodes, podcasts=PODCASTS)
-
-    return render_template('index.html', podcasts=PODCASTS)
+    return render_template('index.html', podcasts=PODCASTS, data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
