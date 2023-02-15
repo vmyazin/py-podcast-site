6f3c9ab3d3d74694772d43882854a8e08baa7383
Vasily Myazin <vm@konspiredesign.com>
2023-02-15 12:37:48 -0300

    Add more podcasts to the config

--- config/podcasts_list.py
+++ config/podcasts_list.py
@@ -1,4 +1,7 @@
 PODCASTS = [
     'http://podcasternews.com/feed/',
-    'https://www.techlifepodcast.com/podcast-feed.xml'
+    'https://www.techlifepodcast.com/podcast-feed.xml',
+    'http://origin.podnews.net/rss/',
+    'http://feeds.feedburner.com/ThisWeekInPodcasting',
+    'https://theaudacitytopodcast.com/feed/'
 ]
